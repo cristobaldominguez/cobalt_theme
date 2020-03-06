06e083b10e1e77c3907a450fdbfac80230e43973
Cristobal Dominguez <cristobald@gmail.com>
2020-03-06 11:15:53 -0300

    Avoids Save Duplicated Commands

--- lib/history.zsh
+++ lib/history.zsh
@@ -27,13 +27,16 @@ esac
 
 ## History file configuration
 [ -z "$HISTFILE" ] && HISTFILE="$HOME/.zsh_history"
-[ "$HISTSIZE" -lt 50000 ] && HISTSIZE=50000
-[ "$SAVEHIST" -lt 10000 ] && SAVEHIST=10000
+[ "$HISTSIZE" -lt 10000000 ] && HISTSIZE=10000000
+[ "$SAVEHIST" -lt 10000000 ] && SAVEHIST=10000000
 
 ## History command configuration
-setopt extended_history       # record timestamp of command in HISTFILE
-setopt hist_expire_dups_first # delete duplicates first when HISTFILE size exceeds HISTSIZE
-setopt hist_ignore_dups       # ignore duplicated commands history list
-setopt hist_ignore_space      # ignore commands that start with space
-setopt hist_verify            # show command with history expansion to user before running it
-setopt share_history          # share command history data
+setopt extended_history        # record timestamp of command in HISTFILE
+setopt hist_expire_dups_first  # delete duplicates first when HISTFILE size exceeds HISTSIZE
+setopt hist_ignore_dups        # ignore duplicated commands history list
+setopt hist_ignore_space       # ignore commands that start with space
+setopt hist_find_no_dups       # Do not display a line previously found.
+setopt hist_save_no_dups       # Don't write duplicate entries in the history file.
+setopt hist_verify             # show command with history expansion to user before running it
+setopt inc_append_history      # add commands to HISTFILE in order of execution
+setopt share_history           # share command history data
